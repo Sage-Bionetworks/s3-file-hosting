560ef22ea69afcb1c10c5f7c76c170ccb0c80924
bhoff <bruce.hoff@sagebase.org>
2023-05-04 18:23:33 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from serverless_backend.serverless_backend_stack import ServerlessBackendStack
+import helpers
+
+app = cdk.App(context=helpers.get_deployment_context())
+ServerlessBackendStack(app, "ServerlessBackendStack",
+    )
+
+app.synth()
